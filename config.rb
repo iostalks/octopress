c3449f5e97dfb05feb7b5084b3ec78bf659d92c1
xfjiang@idelan.cn <xfjiang@idelan.cn>
2015-07-07 10:52:50 +0800

    提交

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/git@github.com:iostalks/"
+http_images_path = "/git@github.com:iostalks/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/git@github.com:iostalks/fonts"
+css_dir = "public/git@github.com:iostalks/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
